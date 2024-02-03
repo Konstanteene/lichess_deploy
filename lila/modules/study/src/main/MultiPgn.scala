ef87ce6980896c64f062a81e34e65968c1e7cf05
Kostiantyn__ <kostyasend02@gmail.com>
2024-02-03 19:53:01 +0200

    add project

---
+++ lila/modules/study/src/main/MultiPgn.scala
@@ -0,0 +1,20 @@
+package lila.study
+
+import chess.format.pgn.PgnStr
+import lila.common.config.Max
+
+case class MultiPgn(value: List[PgnStr]) extends AnyVal:
+
+  def toPgnStr = PgnStr(value mkString "\n\n")
+
+object MultiPgn:
+
+  private[this] val splitPat = """\R{2}(?=\[)""".r.pattern
+
+  def split(str: PgnStr, max: Max) = MultiPgn:
+    PgnStr from splitPat
+      .split(str.value)
+      .view
+      .filter(_.nonEmpty)
+      .take(max.value)
+      .toList
